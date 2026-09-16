d4ebe07e8cebd5ca8c3af23d05b8083ce0d1e0cc
darinsteve08-blip <darinsteve08@gmail.com>
2026-09-16 15:43:08 -0500

    actualizar

--- inventario.spring/src/main/resources/static/js/login.js
+++ inventario.spring/src/main/resources/static/js/login.js
@@ -31,7 +31,7 @@ document.getElementById("formLogin").addEventListener("submit", function(event) 
         password: passwordInput
     };
 
-    fetch('https://inventario-api-15v1.onrender.com/api/usuarios/login', {
+    fetch('https://inventario-api-15v1.onrender.com/api/usuarios/index', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(credenciales)
